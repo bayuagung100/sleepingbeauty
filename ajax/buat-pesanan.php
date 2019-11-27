c3427989c9ca05840d7317e3279657640c43daaf
bayuagung100 <bayuagung100@gmail.com>
2019-11-27 16:27:13 +0700

    revisi lagi

--- ajax/buat-pesanan.php
+++ ajax/buat-pesanan.php
@@ -87,4 +87,39 @@ while ($data = mysqli_fetch_array ($query)) {
         $deltemp = mysqli_query($mysqli,"DELETE FROM temp_cart WHERE id='$tid' AND id_session='$sesi' ");
     }
 }
+
+    $to = $email;
+    $subject = "HTML email";
+
+    $message = "
+    <html>
+    <head>
+    <title>HTML email</title>
+    </head>
+    <body>
+    <p>This email contains HTML Tags!</p>
+    <table>
+    <tr>
+    <th>Firstname</th>
+    <th>Lastname</th>
+    </tr>
+    <tr>
+    <td>John</td>
+    <td>Doe</td>
+    </tr>
+    </table>
+    </body>
+    </html>
+    ";
+
+    // Always set content-type when sending HTML email
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+    // More headers
+    $headers .= 'From: <sbeauty@erolperkasamandiri.co.id>' . "\r\n";
+    $headers .= 'Cc: smileyoudontcry100@gmail.com' . "\r\n";
+
+    mail($to,$subject,$message,$headers);
+
 ?>
--- website/terimakasih.php
+++ website/terimakasih.php
@@ -29,115 +29,9 @@ $ongkir = $pecah2[2];
 $ekspedisi = strtoupper($data['ekspedisi']);
 $kecamatan = $data['kecamatan'];
 $total_berat = $data['total_berat'];
-if ($jml > 0) {    
-    $to = $email;
-    $subject = "HTML email";
-
-    $message = "
-    <html>
-    <head>
-    <title>HTML email</title>
-    </head>
-    <body>
-    <p>This email contains HTML Tags!</p>
-    <table>
-    <tr>
-    <th>Firstname</th>
-    <th>Lastname</th>
-    </tr>
-    <tr>
-    <td>John</td>
-    <td>Doe</td>
-    </tr>
-    </table>
-    </body>
-    </html>
-    ";
-
-    // Always set content-type when sending HTML email
-    $headers = "MIME-Version: 1.0" . "\r\n";
-    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-    // More headers
-    $headers .= 'From: <sbeauty@erolperkasamandiri.co.id>' . "\r\n";
-    $headers .= 'Cc: smileyoudontcry100@gmail.com' . "\r\n";
-
-    if(mail($to,$subject,$message,$headers)){
-        echo '
-        <script>
-        alert("berhasil");
-        </script>
-        ';
-    } else {
-        echo '
-        <script>
-        alert("gagal");
-        </script>
-        ';
-    }
-
-    // $mail_tagihan = strip_tags(htmlspecialchars($id_pesanan));
-    // $mail_name = strip_tags(htmlspecialchars($nama));
-    // $mail_email_address = strip_tags(htmlspecialchars($email));
-    // $mail_phone = strip_tags(htmlspecialchars($hp));
-
-    // $query2 = mysqli_query($mysqli,"SELECT * FROM pembelian WHERE id_pesanan='$id_pesanan' ");
-    // $dtpem = mysqli_fetch_array ($query2);
-
-    // // Create the email and send the message
-    // $to = $email; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-    // $email_subject = "Tagihan Telah Dibuat";
-    // $email_body = "Halo, $mail_name!\n\n";
-    // $email_body .= "Email ini adalah pemberitahuan Tagihan Anda yang dibuat pada $dtpem[date]\n\n";
-    // $email_body .= "ID Pesanan: $id_pesanan\n\n";
-    // $email_body .= "Order Details: \n\n";
-    // while ($mail = mysqli_fetch_array($query2)) {
-    //     $mip = $mail['id_product'];
-    //     $mj = $mail['qty'];
-    //     $mu = $mail['size'];
-    //     $mc = $mail['color']
-    //     $mquery = mysqli_query($mysqli,"SELECT * FROM product WHERE id='$mip' ");
-    //     while ($dquery = mysqli_fetch_array($mquery)) {
-    //         $dqnp = $dquery['product_name'];
-    //         $dqh = $dquery['product_price'];
-    //         $dqb = $dquery['product_weight'];
-
-    //         $ib = $mj*$dqb;
-    //         $it = $mj*$dqh;
-
-    //         $email_body .= "- $dqnp\n";
-    //         $email_body .= "Ukuran: $mu\n";
-    //         $email_body .= "Warna: $mc\n";
-    //         $email_body .= "Berat: ".$dqb."(gram) x ".$mj." = ".$ib."(gram)\n";
-    //         $email_body .= "Harga: ".rupiah($dqh)." x ".$mj." = ".rupiah($it)."\n\n";
-
-    //     }
-    // }
-    // $email_body .= "Subtotal: ".$subtotal."\n";
-    // $email_body .= "Ongkir: ".rupiah($ongkir)." melalui $ekspedisi $layanan -> $kecamatan @ $total_berat gram\n";
-    // $email_body .= "Payment Method: Bank ".$payment_method." - \n";
-    // $email_body .= "Total: ".rupiah($grandtotal)."\n\n";
-    // $email_body .= "----------------------------------------------------------------------\n\n";
-    // $email_body .= "Anda dapat melakukan pembayaran ke rekening kami, sebagai berikut:\n";
-    // $email_body .= "*Mohon sertakan ID Pesanan, untuk konfirmasi pembayaran.\n\n";
-    // $bankquery = mysqli_query($mysqli,"SELECT * FROM rekening_bank");
-    // while ($b = mysqli_fetch_array($bankquery)) {
-    //     $nama_bank = $b['nama_bank'];
-    //     $no_rek = $b['no_rek'];
-    //     $nama_pemilik = $b['nama_pemilik'];
-
-    //     $email_body .= "- $nama_bank\n";
-    //     $email_body .= "No Rekening: $no_rek\n";
-    //     $email_body .= "a/n: $nama_pemilik\n\n";
-    // }
-    // $email_body .= "\n\n\n";
-    // $email_body .= "Terima Kasih.\n";
-    // $email_body .= "www.sleepingbeauty.co.id";
-    // $headers = "From: sbeauty@erolperkasamandiri.co.id\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-    // $headers .= "Reply-To: $mail_email_address";
-    // mail($to,$email_subject,$email_body,$headers);
+if ($jml > 0) {   
 ?>
-<!-- <div class="container m-t-50 m-b-50">
+<div class="container m-t-50 m-b-50">
     <div class="bor10 p-lr-40 p-t-30 p-b-40">
         <h4 class="text-center mtext-109 cl2">Terima kasih.<br>Pesanan Anda telah dibuat.</h4>
         <hr>
@@ -269,7 +163,7 @@ if ($jml > 0) {
 
         </div>
     </div>
-</div> -->
+</div>
 <?php
 } else {
     echo'

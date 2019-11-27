1a62db33b3226aaa561e57914bb5cdedfdeabcd7
bayuagung100 <bayuagung100@gmail.com>
2019-11-27 16:34:10 +0700

    buat template mail

--- ajax/buat-pesanan.php
+++ ajax/buat-pesanan.php
@@ -89,28 +89,9 @@ while ($data = mysqli_fetch_array ($query)) {
 }
 
     $to = $email;
-    $subject = "HTML email";
+    $subject = "Pesanan Anda telah dibuat!";
 
-    $message = "
-    <html>
-    <head>
-    <title>HTML email</title>
-    </head>
-    <body>
-    <p>This email contains HTML Tags!</p>
-    <table>
-    <tr>
-    <th>Firstname</th>
-    <th>Lastname</th>
-    </tr>
-    <tr>
-    <td>John</td>
-    <td>Doe</td>
-    </tr>
-    </table>
-    </body>
-    </html>
-    ";
+    $message = include "../mail/buat-pesanan.html";
 
     // Always set content-type when sending HTML email
     $headers = "MIME-Version: 1.0" . "\r\n";
